be80eaa0297bfbbbe078150bebb249390bc17556
zerokater <fatbardh_berisha@icloud.com>
2024-05-22 23:55:03 +0200

    PIZZA-UPDATE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,80 @@
-<!-- src/routes/YourPage.svelte -->
-<script context="module">
-    export async function preload() {
-      try {
-        const res = await fetch('/static/pizzen.json');
-        const pizzen = await res.json();
-        return {
-          pizzen
-        };
-      } catch (error) {
-        console.error('Error fetching pizzas:', error);
-        return {
-          error: "Error fetching pizzas"
-        };
-      }
-    }
-  </script>
-  
-  <script>
-    export let pizzen;
-  </script>
-  
-  <h1>Pizzen</h1>
-  
-  {#if pizzen}
-    {#each pizzen as pizza}
-      <div>
-        <h2>{pizza.name}</h2>
-        <p>Description: {pizza.description}</p>
-        <p>Price: {pizza.price}</p>
+<script>
+  let pizzaOpen = false;
+
+  import { slide } from "svelte/transition";
+  function togglePizza(){
+    pizzaOpen = !pizzaOpen;
+  }
+</script>
+
+<main>
+  <div class="pizza" >
+    <div class="container-pizza">
+      <h1>Pizzen</h1>
+      <img src="/images/drop-down.svg" alt="" on:click={togglePizza}>
+    </div>
+    
+      {#if pizzaOpen}
+      <div class="pizzen" transition:slide>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
+        <p>margherita</p>
       </div>
-    {/each}
-  {:else}
-    <p>Hala ska piza njer</p>
-  {/if}
+    {/if}
+
+  </div>
+
+  <h1>Pasta</h1>
+  <h1>Salate</h1>
+</main>
   
+
+
+
+<style>
+
+
+  main{
+      padding: 1rem 0rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+
+  .pizza{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 1rem;
+  }
+
+  .container-pizza{
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1pt solid black;
+  }
+
+  .container-pizza img{
+    width: 48px;
+
+  }
+
+  .pizzen{
+    border: 1pt solid black;
+    padding: 1rem;
+  }
+
+
+
+</style>
